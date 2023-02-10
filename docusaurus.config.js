4a26a864c23a6cf0d6c014231176b862554b2d4a
Xuan <37977109+AprilNEA@users.noreply.github.com>
2023-02-11 01:11:19 +0800

    chore: index is docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -64,7 +65,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'XJTLU Guide',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -77,6 +78,10 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -127,7 +132,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} XJTLU-Geek, Npo. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
