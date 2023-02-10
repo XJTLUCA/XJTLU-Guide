53ba478aa201f31d20a4a8d5f1e9cdc7f1f16828
Xuan <37977109+AprilNEA@users.noreply.github.com>
2023-02-10 17:45:56 +0800

    docs: update `README.md`

--- README.md
+++ README.md
@@ -1,41 +1,34 @@
-# Website
+# XJTLU Guide 西浦生活指南
 
-This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+本仓库是西交利物浦大学生活指南的源码
 
-### Installation
+## 贡献
+### 贡献网站内容
 
-```
-$ yarn
-```
 
-### Local Development
+### 参与网站建设
+#### 安装 Installation
 
-```
-$ yarn start
-```
-
-This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
-
-### Build
-
-```
-$ yarn build
+```shell
+npm install # or yarn
 ```
 
-This command generates static content into the `build` directory and can be served using any static contents hosting service.
+#### 本地预览 Local Development
 
-### Deployment
-
-Using SSH:
-
-```
-$ USE_SSH=true yarn deploy
+```shell
+npm start # or yarn start
 ```
 
-Not using SSH:
+#### 构建 Build
 
-```
-$ GIT_USER=<Your GitHub username> yarn deploy
+```shell
+npm build # or yarn build
 ```
 
-If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
+
+#### 部署 Deployment
+
+
+-----
+
+Power by [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'XJTLU Guide',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://xjtlu.best',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'XJTLU-Geek', // Usually your GitHub org/user name.
+  projectName: 'XJTLU-Guide', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -28,8 +28,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans','en'],
   },
 
   presets: [
